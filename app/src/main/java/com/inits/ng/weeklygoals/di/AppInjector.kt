298281b9bc3ff1e50b6ebbe904d39b83d8854269
petyr47 <anekepeter47@gmail.com>
2020-01-06 13:23:43 +0100

    setup appInjector

--- app/src/main/java/com/inits/ng/weeklygoals/di/AppInjector.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/di/AppInjector.kt
@@ -1,2 +1,17 @@
 package com.inits.ng.weeklygoals.di
 
+import com.inits.ng.weeklygoals.data.AppDatabase
+import com.inits.ng.weeklygoals.goals.GoalRepository
+import com.inits.ng.weeklygoals.goals.GoalViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val repositoryModule = module {
+    single {  AppDatabase.getInstance(get()) }
+    single { GoalRepository(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { GoalViewModel(get()) }
+}
