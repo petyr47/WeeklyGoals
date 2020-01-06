276d292a85f2fafec0d5bac5a287e06963397379
petyr47 <anekepeter47@gmail.com>
2020-01-06 10:57:03 +0100

    create model for week

---
+++ app/src/main/java/com/inits/ng/weeklygoals/data/Week.kt
@@ -0,0 +1,14 @@
+package com.inits.ng.weeklygoals.data
+
+data class Week(
+
+    val startDate : String,
+
+    val endDate: String,
+
+    val startStamp: Long,
+
+    val endStamp: Long,
+
+    val index: Int
+)
