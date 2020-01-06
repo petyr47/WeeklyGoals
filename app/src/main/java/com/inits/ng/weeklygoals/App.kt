73d8d7d356c9ce20b112996f0337e01231ade04c
petyr47 <anekepeter47@gmail.com>
2020-01-06 13:23:26 +0100

    start Koin

--- app/src/main/java/com/inits/ng/weeklygoals/App.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/App.kt
@@ -1,15 +1,23 @@
 package com.inits.ng.weeklygoals
 
 import android.app.Application
+import com.inits.ng.weeklygoals.di.repositoryModule
+import com.inits.ng.weeklygoals.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-
+        initKoin()
     }
 
     private fun initKoin(){
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(repositoryModule, viewModelModule))
+        }
     }
 }
