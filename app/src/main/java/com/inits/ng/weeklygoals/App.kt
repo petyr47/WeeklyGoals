3c386c0febdb416280eec59091d3926028be43d1
petyr47 <anekepeter47@gmail.com>
2020-01-06 12:42:45 +0100

    create App class

---
+++ app/src/main/java/com/inits/ng/weeklygoals/App.kt
@@ -0,0 +1,15 @@
+package com.inits.ng.weeklygoals
+
+import android.app.Application
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+    private fun initKoin(){
+    }
+}
