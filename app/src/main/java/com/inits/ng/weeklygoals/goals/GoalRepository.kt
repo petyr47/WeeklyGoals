756df92a3233474d5d8c62c9ae04bc5841f7106b
petyr47 <anekepeter47@gmail.com>
2020-01-10 10:57:40 +0100

    setup GoalRepository

--- app/src/main/java/com/inits/ng/weeklygoals/goals/GoalRepository.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/goals/GoalRepository.kt
@@ -1,9 +1,56 @@
 package com.inits.ng.weeklygoals.goals
 
+import androidx.lifecycle.LiveData
 import com.inits.ng.weeklygoals.data.AppDatabase
+import com.inits.ng.weeklygoals.data.Goal
+import kotlin.Exception
 
-class GoalRepository(private val db : AppDatabase) {
+class GoalRepository(private val db: AppDatabase) {
 
     private val dao = db.dao()
+
+
+    suspend fun fetchAllGoals(): List<Goal> =
+        try {
+            dao.fetchAllGoals()
+        } catch (e: Exception) {
+            emptyList()
+        }
+
+
+    suspend fun insertGoal(goal: Goal): Boolean =
+        try {
+            dao.saveGoal(goal)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+
+    fun observeGoals(): LiveData<List<Goal>>? {
+        try {
+            return dao.observeAllGoals()
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+
+    suspend fun updateGoal(goal: Goal): Boolean =
+        try {
+            dao.updateGoal(goal)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+
+    suspend fun deleteGoal(goal: Goal): Boolean =
+        try {
+            dao.removeGoal(goal)
+            true
+        } catch (e: Exception) {
+            false
+        }
 
 }
