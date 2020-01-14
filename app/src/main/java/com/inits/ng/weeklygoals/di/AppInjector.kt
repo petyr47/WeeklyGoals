d3327021b565ae799cc1e6eb4af0e2201f926a91
petyr47 <anekepeter47@gmail.com>
2020-01-14 13:25:04 +0100

    add inputViewModel to viewmodel module

--- app/src/main/java/com/inits/ng/weeklygoals/di/AppInjector.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/di/AppInjector.kt
@@ -3,6 +3,7 @@ package com.inits.ng.weeklygoals.di
 import com.inits.ng.weeklygoals.data.AppDatabase
 import com.inits.ng.weeklygoals.goals.GoalRepository
 import com.inits.ng.weeklygoals.goals.GoalViewModel
+import com.inits.ng.weeklygoals.goals.InputViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -14,4 +15,5 @@ val repositoryModule = module {
 
 val viewModelModule = module {
     viewModel { GoalViewModel(get()) }
+    viewModel { InputViewModel(get()) }
 }
