08cf11278dd82e904bfac363dee25e53cdb74b33
petyr47 <anekepeter47@gmail.com>
2020-01-15 14:02:03 +0100

    add Settings Activity

---
+++ app/src/main/java/com/inits/ng/weeklygoals/settings/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.inits.ng.weeklygoals.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inits.ng.weeklygoals.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
