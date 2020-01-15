73a928540ea3b7bd7f2b0fcf9dcfbb6a0f85c34e
petyr47 <anekepeter47@gmail.com>
2020-01-15 11:07:08 +0100

    setup inputviewmodel add validation add function for inserting goal to db

--- app/src/main/java/com/inits/ng/weeklygoals/goals/InputViewModel.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/goals/InputViewModel.kt
@@ -1,9 +1,13 @@
 package com.inits.ng.weeklygoals.goals
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.util.*
+import androidx.lifecycle.viewModelScope
+import com.inits.ng.weeklygoals.data.Goal
+import com.inits.ng.weeklygoals.data.Week
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import com.inits.ng.weeklygoals.utils.*
 
 class InputViewModel(private val repository: GoalRepository): ViewModel() {
 
@@ -20,6 +24,56 @@ class InputViewModel(private val repository: GoalRepository): ViewModel() {
     val endStamp = MutableLiveData<Long>()
 
     val displayDate = MutableLiveData<String>()
+
+    val success = MutableLiveData<Boolean>()
+
+    init {
+        success.value = false
+        header.value = "Add Goal"
+    }
+
+
+    private fun validateFields() : Boolean{
+        if(title.value.isNullOrBlank()){
+            errorMessage.value = "Title cannot be blank"
+            return false
+        }
+        if (description.value.isNullOrBlank()){
+            errorMessage.value = "Description cannot be blank"
+            return false
+        }
+        if (displayDate.value.isNullOrBlank()){
+            errorMessage.value = "Please select date range"
+            return false
+        }
+        return true
+    }
+
+
+    fun saveGoalClicked(){
+        if(validateFields()){
+            val week = Week(
+                startDate = makeDateFromEpoch(startStamp.value!!),
+                endDate = makeDateFromEpoch(endStamp.value!!),
+                endStamp = endStamp.value!!,
+                startStamp = startStamp.value!!
+            )
+            val goal = Goal(
+                week = week,
+                title = title.value!!,
+                message = description.value!!
+            )
+            viewModelScope.launch (Dispatchers.IO) {
+                val result = repository.insertGoal(goal)
+                success.postValue(result)
+                if (result){
+                    errorMessage.postValue("Goal saved!")
+                }else{
+                    errorMessage.postValue("Goal could not be saved!")
+                }
+            }
+        }
+    }
 
 
 
