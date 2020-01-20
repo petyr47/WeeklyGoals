462fd63ec6bb2266904a487ef4d1d32ff5fdfa6f
petyr47 <anekepeter47@gmail.com>
2020-01-20 10:08:35 +0100

    register work in app onCreate

--- app/src/main/java/com/inits/ng/weeklygoals/App.kt
+++ app/src/main/java/com/inits/ng/weeklygoals/App.kt
@@ -1,10 +1,15 @@
 package com.inits.ng.weeklygoals
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.inits.ng.weeklygoals.di.repositoryModule
 import com.inits.ng.weeklygoals.di.viewModelModule
+import com.inits.ng.weeklygoals.workmanager.GoalNotificationWorker
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import java.util.concurrent.TimeUnit
 
 class App: Application() {
 
@@ -12,6 +17,7 @@ class App: Application() {
         super.onCreate()
 
         initKoin()
+        registerWorker()
     }
 
     private fun initKoin(){
@@ -19,5 +25,20 @@ class App: Application() {
             androidContext(this@App)
             modules(listOf(repositoryModule, viewModelModule))
         }
+    }
+
+    private fun registerWorker(){
+        val work = PeriodicWorkRequestBuilder<GoalNotificationWorker>(16, TimeUnit.MINUTES)
+            .addTag("goal-notif-work")
+            .build()
+
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "goal-notif-work",
+            ExistingPeriodicWorkPolicy.REPLACE,
+            work
+        )
+
+
     }
 }
